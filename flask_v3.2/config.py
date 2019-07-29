d22ac92949bbb7d734319b77660661165a4614d1
Tattao <shihantao5@hotmail.com>
2019-07-29 14:15:09 +0800

    Update config.py

--- flask_v3.2/config.py
+++ flask_v3.2/config.py
@@ -12,7 +12,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Smy Flask Web]'
-    FLASKY_MAIL_SENDER = 'Muyu Shen <502553633@qq.com>'
+    FLASKY_MAIL_SENDER = '*********@***.***'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 50
 
